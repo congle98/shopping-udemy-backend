3c0c4c582a9ac8fb4b61445568c7fde8b9d8ffbc
Le Thanh Cong (PM) <conglt@nms.vn>
2021-09-29 17:33:11 +0700

    khoi dong

---
+++ src/main/java/com/example/shopbackend/config/MyDataRestConfig.java
@@ -0,0 +1,23 @@
+package com.example.shopbackend.config;
+
+import com.example.shopbackend.entity.Product;
+import com.example.shopbackend.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//chỉ cho lấy ra chứ ko  cho thêm xóa sửa
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.POST,HttpMethod.DELETE,HttpMethod.PUT};
+        config.getExposureConfiguration().forDomainType(Product.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+        config.getExposureConfiguration().forDomainType(ProductCategory.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
+}
